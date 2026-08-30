f787ee4966ce860d54caf372ddf6fd41a17b3af1
Quasin <nulc@ya.ru>
2026-08-30 08:20:28 +0800

    Add yarn

--- installdev.sh
+++ installdev.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
+set -e
+
+export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
 
 sudo apt update
 sudo apt install -y curl build-essential
 curl -fsSL https://deb.nodesource.com/setup_24.x | sudo -E bash -
 sudo apt install -y nodejs
+
 node -v
 npm -v
 
+sudo npm install -g corepack npm@latest
+corepack enable
+
+yarn --version
+#yarn init -y
+yarn config set --home enableTelemetry 0
